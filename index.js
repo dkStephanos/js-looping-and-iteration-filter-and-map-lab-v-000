ab13e66547417be3e9600ee55c164e1823e7c7c2
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 18:32:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  const collection = [];
+
+  for (const driver of drivers) {
+    if (driver.revenue > revenue) {
+      collection.push(driver)
+    }
+  }
+
+  return collection
+}
